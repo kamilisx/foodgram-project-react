2b8d4198c8ed616feb968c42993c91058279e18a
Камиль Исхаков <kamiliskhakov@MacBook-Air-Kamil.local>
2023-11-23 20:57:11 +0400

    Add Actions

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    """Custom pagination."""
+    """Custom pagination"""
     page_size: int = 6
     page_size_query_param: str = "limit"
     max_page_size: int = 20
