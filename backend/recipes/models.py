5fd71c546cdacb91526c782a4ea3cad56cd9187f
Камиль Исхаков <kamiliskhakov@MacBook-Air-Kamil.local>
2023-11-27 02:27:28 +0400

    second  commit

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -115,8 +115,8 @@ class Ingredient(models.Model):
     )
 
     class Meta:
-        verbose_name = "Ингридиент"
-        verbose_name_plural = "Ингридиенты"
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
         ordering = ("-pk",)
         constraints = [
             models.UniqueConstraint(
